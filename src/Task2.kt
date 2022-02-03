5f1a79544cc3cc3eb7bb83250f5450bdb16a6c00
Айша <araevaaisha13@mail.ru>
2022-02-03 22:22:13 +0300

    Homework task1 & task2

---
+++ src/Task2.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val name = "Aisha"
+    val age = 26
+    val balance = 101
+    val gluten = false
+    println(
+        """
+        name: $name
+        age: $age
+        balance: $balance 
+        gluten: $gluten 
+        """
+    )
+}
